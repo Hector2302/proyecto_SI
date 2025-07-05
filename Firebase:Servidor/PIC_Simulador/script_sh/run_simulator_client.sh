143049593bc78828173683e6cdb12657e405a3a7
Héctoralv <alvaradomareshector@gmail.com>
2025-07-05 14:33:25 -0600

    nueva version, eliminando multiples archivos que ya no se utilizan actualemnte

--- Firebase:Servidor/PIC_Simulador/script_sh/run_simulator_client.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export GOOGLE_APPLICATION_CREDENTIALS="$HOME/proyecto/secure/firebase_decrypted/Service_Account_PIC.json"
-python3 "$HOME/proyecto/scripts_python/simulador_anti_incendios.py"
